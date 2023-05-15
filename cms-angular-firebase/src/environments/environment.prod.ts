dd9269f72878e9ee0920aa42f42a68859ae6b498
Lilit Harutyunyan <harutyunianlilit@gmail.com>
2023-05-16 00:41:50 +0400

    first commit

---
+++ cms-angular-firebase/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDGzdVlONxQc4ulMR7ZUCNRdHyTUB7zIOc",
+    authDomain: "cms-database-534d2.firebaseapp.com",
+    databaseURL: "https://cms-database-534d2-default-rtdb.firebaseio.com",
+    projectId: "cms-database-534d2",
+    storageBucket: "cms-database-534d2.appspot.com",
+    messagingSenderId: "431544941754",
+    appId: "1:431544941754:web:fd5245a5d7dd9274561683",
+    measurementId: "G-C6BQEZFVCL"
+  }
+};
